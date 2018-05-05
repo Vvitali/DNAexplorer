9b2a7ec450b3c8bdf67a0627e4452e6621aceac7
Vvitali <vvitali26@gmail.com>
2018-05-04 23:47:09 -0400

    body parser installed< API for requesting numerious lines from DNA

--- server.js
+++ server.js
@@ -1,23 +1,36 @@
 let app = require("express")();
 let fs = require('fs');
 var DEBUG = true;
-
-app.get("/", (req, res)=>{
+let PORT = 8080;	
+app.get("/rsids/:number", (req, res)=>{
 	DEBUG && console.log("Page /");
-
 	let parsedDna = [];
 	// fs.readFile("./data/DNA.txt")
 	let lineReader = require('readline').createInterface({
 		input: require('fs').createReadStream('./data/DNA.txt')
 	});
-
-
-	lineReader.on('line', function (line) {
-		parsedDna.push(line.split(" ")); 
+	var counter = 0;
+	lineReader.on('line', function (line) { 
+		let splittedLine = line.split("	");
+		parsedDna.push({
+			rsid: splittedLine[0],
+			chromosome: splittedLine[1],
+			position:splittedLine[2],
+			allele1:splittedLine[3],
+			allele2:splittedLine[4]
+		});  
 	});
-	lineReader.on('end', function (line) { 
+	lineReader.on('close', function (line) { 
 		console.log("Parsing is completed!");
+		let dnaPackage = parsedDna;
+		dnaPackage.length=req.body.number;
+		res.send(dnaPackage);
 		lineReader.close();
-	});
 
+	});
 });
+
+
+app.listen(PORT, ()=>{
+	console.log("Server is running on port:", PORT)
+})
